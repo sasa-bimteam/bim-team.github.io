b57facc09bdd71c686c3e2ca159267b15e51206c
Sasa Jovanovic <studio@bim-team.co>
2021-05-15 21:00:31 +0200

    Add content to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,31 +4,28 @@ import React from 'react'
 const Footer = props => (
   <footer id="footer">
     <section>
-      <h2>Aliquam sed mauris</h2>
+      <h2>Kavica. Kadarkoli.</h2>
       <p>
-        Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat
-        magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras
-        aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae
-        tincidunt odio.
+        Kako naprej? Imate vprašanja? Potrebujete več informacij? Želite na pijačo?
       </p>
       <ul className="actions">
         <li>
           <Link to="/generic" className="button">
-            Learn More
+            Kontaktirajte nas
           </Link>
         </li>
       </ul>
     </section>
     <section>
-      <h2>Etiam feugiat</h2>
+      <h2>Kontakt</h2>
       <dl className="alt">
-        <dt>Address</dt>
-        <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
-        <dt>Phone</dt>
-        <dd>(000) 000-0000 x 0000</dd>
+        <dt>Naslov</dt>
+        <dd>Ljubljana, Slovenija, Europe, Earth, Solar system :)</dd>
+        <dt>Telefon</dt>
+        <dd>+386 68 642 687</dd>
         <dt>Email</dt>
         <dd>
-          <a href="https://codebushi.com">information@untitled.tld</a>
+          <a href="mainto:studio@bim-team.co">studio@bim-team.co</a>
         </dd>
       </dl>
       <ul className="icons">
@@ -50,23 +47,10 @@ const Footer = props => (
             <span className="label">Instagram</span>
           </a>
         </li>
-        <li>
-          <a
-            href="https://github.com/codebushi/gatsby-starter-stellar"
-            className="icon fa-github alt"
-          >
-            <span className="label">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://codebushi.com" className="icon fa-dribbble alt">
-            <span className="label">Dribbble</span>
-          </a>
-        </li>
       </ul>
     </section>
     <p className="copyright">
-      &copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.
+      copyright&copy; 2021 by BIM Team, all rights reserved worldwide.
     </p>
   </footer>
 )
