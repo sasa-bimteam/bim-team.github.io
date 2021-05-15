8c497deb9229f1acc24919f15a114bd8a6cbcc0e
Sasa Jovanovic <studio@bim-team.co>
2021-05-15 20:40:26 +0200

    Add space

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
         <h1>BIM Team</h1>
-        <p>BIM | Architecture | Design<br />
+        <p>BIM | Architecture | Design <br />
         Narejeno z ♥ v Sloveniji</p>
         <p>POSTANITE DEL DIGITALNE PREOBRAZBE<br />
         IZKORISTITE CELOTNI POTENCIAL BIM PROCESA</p>
