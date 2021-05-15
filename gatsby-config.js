32361e2adf906188098b82201b9d23ae694bce02
Sasa Jovanovic <studio@bim-team.co>
2021-05-15 17:52:11 +0200

    Update header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
+    title: "BIM Team",
+    author: "BIM Team",
+    description: "BIM | Architecture | Design"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,10 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <h1>BIM Team</h1>
+        <p>BIM | Architecture | Design<br />
+        Narejeno z ♥ v Sloveniji</p>
+        <p>POSTANITE DEL DIGITALNE PREOBRAZBE IZKORISTITE CELOTNI POTENCIAL BIM PROCESA</p>
     </header>
 )
 
