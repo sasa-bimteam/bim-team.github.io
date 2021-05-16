590480d1802eef9fc2626546c9314219c1d0394b
Sasa Jovanovic <studio@bim-team.co>
2021-05-16 19:26:19 +0200

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,10 @@ import logo from '../assets/images/logo.png';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>BIM Team</h1>
-        <p>BIM | Architecture | Design <br />
-        Narejeno z ♥ v Sloveniji</p>
-        <p>POSTANITE DEL DIGITALNE PREOBRAZBE<br />
-        IZKORISTITE CELOTNI POTENCIAL BIM PROCESA</p>
+        <p>BIM | Architecture | Design</p>
+        <p>Narejeno z ♥ v Sloveniji</p>
+        <p>POSTANITE DEL DIGITALNE PREOBRAZBE</p>
+        <p>IZKORISTITE CELOTNI POTENCIAL BIM PROCESA</p>
     </header>
 )
 
