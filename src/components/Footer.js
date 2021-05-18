00682ad0b978a681f3a37bdc5aff5b8f1a2958b6
Sasa Jovanovic <studio@bim-team.co>
2021-05-18 19:37:27 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = props => (
   <footer id="footer">
-    <section> 
+    <section id="levo"> 
       <h2>Kavica. Kadarkoli.</h2>
       <p>
         Kako naprej? Imate vprašanja? <br />
@@ -17,7 +17,7 @@ const Footer = props => (
         </li>
       </ul>
     </section>
-    <section>
+    <section id="desno">
       <h2>Kontakt</h2>
       <dl className="alt">
         <dt>Naslov</dt>
