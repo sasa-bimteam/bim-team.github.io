f64fcd28940dcda6ce3d67485de7f87dcc0ceb63
Sasa Jovanovic <studio@bim-team.co>
2021-05-21 18:05:05 +0200

    BIM Arhitektura Dizajn

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import logo from '../assets/images/logo.png';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <p>BIM | Architecture | Design</p>
+        <p>BIM | Arhitektura | Dizajn</p>
         <p>Narejeno z ♥ v Sloveniji</p>
         <p>POSTANITE DEL DIGITALNE PREOBRAZBE</p>
         <p>IZKORISTITE CELOTNI POTENCIAL</p>
