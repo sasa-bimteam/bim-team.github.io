c3415c5f13c3f019b03742e6bae80cb53fd4608d
Sasa Jovanovic <studio@bim-team.co>
2021-05-22 00:02:48 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo1><img src={logo} alt="" /></span>
+        <span className="logo><img src={logo} alt="" /></span>
     </header>
 )
 
