87caa4f53e89de679295759490450b3d526681b9
Sasa Jovanovic <studio@bim-team.co>
2021-05-21 23:46:33 +0200

    obrisan text iz hedera

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,6 @@ import logo from '../assets/images/logo.png';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <p>BIM | Arhitektura | Dizajn</p>
-        <p>Narejeno z ♥ v Sloveniji</p>
-        <p>POSTANITE DEL DIGITALNE PREOBRAZBE</p>
-        <p>IZKORISTITE CELOTNI POTENCIAL</p>
-        <p>BIM PROCESA</p>
     </header>
 )
 
