72e7e916e9852f403720aa0cdcecc3105d95ed33
Sasa Jovanovic <studio@bim-team.co>
2021-05-22 00:07:36 +0200

    unazad

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import logo from '../assets/images/logo.png';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
+     
     </header>
 )
 
