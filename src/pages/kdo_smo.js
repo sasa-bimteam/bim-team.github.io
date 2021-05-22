794a41a25e7f39631fdc7f58325dbd3c9b9d0d06
Sasa Jovanovic <studio@bim-team.co>
2021-05-22 17:41:50 +0200

    Update kdo_smo.js

--- src/pages/kdo_smo.js
+++ src/pages/kdo_smo.js
@@ -26,7 +26,7 @@ class KdoSmo extends React.Component {
               <div className="content">
                 <header className="major">
                   <h2>Saša Jovanović</h2>
-                  <h4>Direktorica | Arhitektka | Certificirana GRAPHISOFT BIM Vodja</h4>
+                  <h7>Direktorica | Arhitektka | Certificirana GRAPHISOFT BIM Vodja</h7>
                 </header>
                 <p>
                 Saša kot arhitektka že več kot 11 let deluje na področju arhitekture,
