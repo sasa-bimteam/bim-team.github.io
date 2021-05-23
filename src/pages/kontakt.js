6aa80824e5c14f39131096b72ccb90c280e86ff3
Sasa Jovanovic <studio@bim-team.co>
2021-05-23 02:51:18 +0200

    presledek Storitve

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -16,7 +16,13 @@ class Kontakt extends React.Component {
           <section id="content" className="main">
             <span className="image main"><img src={pic01} alt="" /></span>
             <h2>BIM storitve</h2>
-            <p>Prehod na pristop BIM predstavlja večji zalogaj, tako finančni, tehnični kot organizacijski. Za uspešno implementacijo potrebujete podporo vodstva in pripravljenost zaposlenih sprejemati spremembe. Pomembno je, da se pri uvajanju ne ustavimo, saj končne prednosti uporabe BIM tehnologije večkratno nadomestijo vložen čas in denar. Z veseljem vam pomagamo z naslednjimi storitvami:
+            <p>Prehod na pristop BIM predstavlja večji zalogaj, tako finančni, tehnični kot organizacijski. 
+            <br />
+            Za uspešno implementacijo potrebujete podporo vodstva in pripravljenost zaposlenih sprejemati spremembe. Pomembno je, da se pri uvajanju ne ustavimo, saj končne prednosti uporabe BIM tehnologije večkratno nadomestijo vložen čas in denar.
+            <br />
+            <br />
+            Z veseljem vam pomagamo z naslednjimi storitvami:
+            <br />
             <br />
             - BIM projektno vodenje z uporabo platforme PLANNERLY - izobraževanja in svetovanja
             <br />
