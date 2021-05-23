b94332560b825fd2f4f7c98a48e75c1fed26d92f
Sasa Jovanovic <studio@bim-team.co>
2021-05-23 19:58:31 +0200

    promena headera test

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from 'react'
 import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
-    <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
+    <header id="header"></header>
+        <div id="main">
+          <section id="content" className="main">
+            <span className="image main"><img src={logo} alt="" /></span>
      
     </header>
 )
