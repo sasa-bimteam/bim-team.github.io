f0bc86b5cb38ab684c51c820cd36954cb59cae55
Sasa Jovanovic <studio@bim-team.co>
2021-05-23 16:18:00 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ const Footer = props => (
         <li>
           <a
             href="https://www.linkedin.com/in/sasajovanovic78/"
-            className="icon fa-linkedin alt"
+            className="icon fab fa-linkedin-in alt"
           >
             <span className="label">Linkedin</span>
           </a>
