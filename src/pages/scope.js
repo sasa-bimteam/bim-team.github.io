82c79f894ec91ca602599c608ac2aa941a8eca25
Sasa Jovanovic <studio@bim-team.co>
2021-05-27 22:04:53 +0200

    scobe bez teksta ispod slike

--- src/pages/scope.js
+++ src/pages/scope.js
@@ -15,9 +15,7 @@ class Scope extends React.Component {
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={picScope} alt="" /></span>
-            <h2>SCOPE</h2>
-            <strong>Visual BIM scopes that everyone can understand</strong>
-            <p>A collaborative framework to define ISO 19650 requirements for Geometry, Documentation and Information (Level of Information Need BS EN 17412-1).</p>
+           
 
             <a href="https://www.plannerly.com/scope/?utm_source=yt5ep" target="_blank" class="button special" rel="noopener noreferrer">
               Začni zdaj
@@ -36,3 +34,6 @@ class Scope extends React.Component {
 }
 
 export default Scope
+///<h2>SCOPE</h2>
+///<strong>Visual BIM scopes that everyone can understand</strong>
+///<p>A collaborative framework to define ISO 19650 requirements for Geometry, Documentation and Information (Level of Information Need BS EN 17412-1).</p>
