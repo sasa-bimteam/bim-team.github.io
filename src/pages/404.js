c1914fe0cfbd2f5361e48f8aeaae0ee7c046951f
Sasa Jovanovic <studio@bim-team.co>
2021-05-27 18:01:59 +0200

    404 u contract page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,39 @@
+import { Link } from 'gatsby'
 import React from 'react'
+import Helmet from 'react-helmet'
+
 import Layout from '../components/layout'
+import picContract from '../assets/images/BIM-Contract-Documents-Complete.jpeg'
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+class Plan extends React.Component {
+  render() {
 
-export default NotFoundPage
+    return (
+      <Layout>
+        <Helmet title="Contract" />
+        <header id="header"></header>
+        <div id="main">
+          <section id="content" className="main">
+            <span className="image main"><img src={picContract} alt="" /></span>
+            <h2>CONTRACT</h2>
+            <strong>Simple BIM Execution Planning</strong>
+            <p>Drag and drop from hundreds of free BEP templates to quickly create and agree on your BIM standards, processes and contracts.</p>
+
+            <a href="https://www.plannerly.com/contract/?utm_source=yt5ep" target="_blank" class="button special" rel="noopener noreferrer">
+              Začni zdaj
+            </a>
+
+            <span>&nbsp;</span>
+
+            <Link to="/" className="button">
+              Nazaj
+            </Link>
+
+          </section>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Plan
