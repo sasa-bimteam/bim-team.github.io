656c02ca312642ec083972e70e345921db883b26
Sasa Jovanovic <studio@bim-team.co>
2021-05-29 23:53:41 +0200

    logo zamena

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.png';
+import logo from '../assets/images/logo1.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
