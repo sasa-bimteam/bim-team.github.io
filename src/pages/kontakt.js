b5c5898e990bd7e527761a7162c8e88af13b7ae8
Sasa Jovanovic <studio@bim-team.co>
2021-06-01 16:46:36 +0200

    Update kontakt.js

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -46,7 +46,7 @@ class Kontakt extends React.Component {
               <Link to="/" className="button">
                 Nazaj
               </Link>
-              <br />
+              <br></br>
               
             </div>
             
